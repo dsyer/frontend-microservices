e3189d7a0a13ea0d4cd96984dceb668c8989ab32
Dave Syer <dsyer@pivotal.io>
2017-03-02 11:22:57 +0000

    Add some tests

--- thymeleaf-gateway/src/test/java/com/example/GatewayApplicationTests.java
+++ thymeleaf-gateway/src/test/java/com/example/GatewayApplicationTests.java
@@ -2,15 +2,32 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class GatewayApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/")).andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("ng-include")));
+    }
 
 }
--- thymeleaf-resource/src/test/java/com/example/ResourceApplicationTests.java
+++ thymeleaf-resource/src/test/java/com/example/ResourceApplicationTests.java
@@ -2,15 +2,34 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureRestDocs(outputDir="target/snippets")
+@AutoConfigureMockMvc
 public class ResourceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/resource")).andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString("Hello World")))
+                .andDo(document("resource"));
+    }
 
 }
